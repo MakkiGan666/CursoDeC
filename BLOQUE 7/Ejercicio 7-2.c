3407c74f4e5785064d8c904f49f82e38c2b2b346
MakkiGan <sebachamorro2102@gmail.com>
2023-06-14 04:06:51 -0300

    Act 14/06

--- BLOQUE 7/Ejercicio 7-2.c
+++ BLOQUE 7/Ejercicio 7-2.c
@@ -7,14 +7,48 @@
 void aleatorio (int *vector, int N);
 
 int main(){
-     
+    int i, j, k; 
     int N = 10;
     int *vector;
 
-    vector = (int*)malloc(N*ziseof(int));
+    vector = (int*)malloc(N*sizeof(int));
     if (vector == NULL){
         printf("No se ha podido reservar espacio de memoria.\n");
     } else {
+        aleatorio(vector, N);
+
+        for ( i = 0; i < N; i++){
+            j = i + 1;
+            while (j < N){       
+                
+                if (*(vector + 1) == *(vector+j)){   
+                    
+                    for( k = j; k+1 < N; k++){
+                        *(vector + k) = *(vector+k+1);
+                    }
+                    
+                    N--;
+                
+                }else{
+                    j++;
+                }
+                
+            }
+            
+        }
+        vector = (int*)realloc(vector,N*sizeof(int));
+        
+        if (vector == NULL){
+            printf("No se a podido reservar el espacio de memoria.\n");
+        }
+        
+        else {
+            for ( i = 0; i < N; i++){
+                printf("%i, ", *(vector));
+            }
+            printf("\n");
+        }
+        
         
     }
     
